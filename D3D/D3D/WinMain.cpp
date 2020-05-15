3fac466273cd6aa3bc9c3107f43981ef228f7b41
SSPYR0 <jarronblizzard@gmail.com>
2020-05-15 19:29:11 -0400

    window

--- D3D/D3D/WinMain.cpp
+++ D3D/D3D/WinMain.cpp
@@ -5,9 +5,31 @@ int CALLBACK WinMain(
 	HINSTANCE hPrevInstance,
 	LPSTR     lpCmdLine,
 	int       nShowCmd
-) {
+){
+	const auto pClassName = "D3D";
+	//register windows class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+	//create window instance
+	HWND hwnd = CreateWindowEx(
+		0, pClassName, "D3D",
+		WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr);
 
+	ShowWindow(hwnd, SW_SHOW);
 	while (true);
-
 	return 0;
 }
