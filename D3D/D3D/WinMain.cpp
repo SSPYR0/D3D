75cf0e223a2509985069aee5b8d390eb52e39d94
SSPYR0 <jarronblizzard@gmail.com>
2020-05-16 21:44:09 -0400

    msg loop

--- D3D/D3D/WinMain.cpp
+++ D3D/D3D/WinMain.cpp
@@ -1,4 +1,17 @@
+//TODO: Move window creation out of main
+//TODO: msg handling 
+
+#include <d3d.h>
 #include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_CLOSE:
+		PostQuitMessage(69);	
+		break;
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -7,11 +20,12 @@ int CALLBACK WinMain(
 	int       nShowCmd
 ){
 	const auto pClassName = "D3D";
+
 	//register windows class
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -22,6 +36,7 @@ int CALLBACK WinMain(
 	wc.lpszClassName = pClassName;
 	wc.hIconSm = nullptr;
 	RegisterClassEx(&wc);
+
 	//create window instance
 	HWND hwnd = CreateWindowEx(
 		0, pClassName, "D3D",
@@ -29,7 +44,16 @@ int CALLBACK WinMain(
 		200, 200, 640, 480,
 		nullptr, nullptr, hInstance, nullptr);
 
+	//show the window 
 	ShowWindow(hwnd, SW_SHOW);
-	while (true);
+
+	//msg
+	MSG msg;
+	while (GetMessage(&msg,nullptr,0,0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
 	return 0;
+
 }
