ea3edd1b0d7a1448c1d4d4bd6fc0c61158f33551
SSPYR0 <jarronblizzard@gmail.com>
2020-05-17 20:24:09 -0400

    cube pt1

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -111,6 +111,7 @@ void Graphics::DrawTestTriangle(float angle, float x, float y)
 		struct {
 			float x;
 			float y;
+			float z;
 		}pos;
 		
 		struct 
@@ -126,14 +127,17 @@ void Graphics::DrawTestTriangle(float angle, float x, float y)
 	// create vertex buffer (1 2d triangle at center of screen)
 	Vertex vertices[] =
 	{
-		{ 0.0f,0.5f,255,0,0,1 },
-		{ 0.5f,-0.5f,0,255,0,1 },
-		{ -0.5f,-0.5f,0,0,255,1 },
-		{ -0.3f,0.3f,0,0,255,1 },
-		{ 0.3f, 0.3f,0,255,0,1 },
-		{ 0.0f,-0.8f,255,0,0,1 },
+		{-1.0f,-1.0f,-1.0f,255,0,0},
+		{1.0f,-1.0f,-1.0f,0,255,0},
+		{-1.0f,1.0f,-1.0f,0,0,255},
+		{1.0f,1.0f,-1.0f,255,255,0},
+		{-1.0f,-1.0f,1.0f,255,0,255},
+		{1.0f,-1.0f,1.0f,0,255,255},
+		{-1.0f,1.0f,1.0f,0,0,0},
+		{1.0f,1.0f,1.0f,255,255,255},
 
 	};
+	vertices[0].col.g = 255;
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 	D3D11_BUFFER_DESC bd = {};
 	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
@@ -153,10 +157,12 @@ void Graphics::DrawTestTriangle(float angle, float x, float y)
 
 	//create index buffer 
 	const unsigned short indices[] = {
-		0,1,2,
-		0,2,3,
-		0,4,1,
-		2,1,5
+		0,2,1,2,3,1,
+		1,3,5,3,7,5,
+		2,6,3,3,6,7,
+		4,5,7,4,7,6,
+		0,4,2,2,4,6,
+		0,1,4,1,5,4
 	};
 
 	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
@@ -185,7 +191,8 @@ void Graphics::DrawTestTriangle(float angle, float x, float y)
 		{
 			dx::XMMatrixTranspose(dx::XMMatrixRotationZ(angle)*
 								 dx::XMMatrixScaling(3.0f / 4.0f,1.0f,1.0f) *
-								dx::XMMatrixTranslation(x,y,0.0f)
+								dx::XMMatrixTranslation(x,y,4.0f) *
+								dx::XMMatrixPerspectiveLH(1.0f,3.0f/4.0f,0.5f,10.0f)
 							)
 
 			
